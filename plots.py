800ad25cad953a62bad0c3753dedfd54a86b101e
ferdavid1 <fespinosa@ucdavis.edu>
2018-01-18 13:19:34 -0800

    small

---
+++ plots.py
@@ -0,0 +1,4 @@
+from sympy import isprime, primerange, randprime, prime, prevprime, nextprime
+import numpy as np 
+import matplotlib.pyplot as plt 
+
---
+++ prime_slicing.py
@@ -0,0 +1,17 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sympy import isprime, primerange, randprime, prime, prevprime, nextprime
+import mpl_toolkits.mplot3d.axes3d as p3
+
+x,y = list(np.random.randn(300)), list(np.random.randn(300))
+primes = list(primerange(0, len(x))) 
+prime_subx, prime_suby = [x[i] for i in primes], [y[i] for i in primes]
+x_hole = [i for i in x if i not in prime_subx]
+y_hole = [i for i in y if i not in prime_suby]
+
+fig = plt.figure()
+ax = p3.Axes3D(fig)
+ax.scatter(x,y, 6) # also could use ax.scatter
+ax.scatter(prime_subx, prime_suby, 4)
+ax.scatter(x_hole, y_hole, 2)
+plt.show()
